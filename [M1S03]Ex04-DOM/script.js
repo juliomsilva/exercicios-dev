f73ae020a71458ac75ba723895cfd034f2cbc90e
xDominicz <julio.machsilva@gmail.com>
2022-07-17 13:14:11 -0300

    Exercicios DEVinHouse

---
+++ [M1S03]Ex04-DOM/script.js
@@ -0,0 +1,7 @@
+let inserirTexto = document.getElementById("TextoAqui");
+
+inserirTexto.innerHTML += "Texto ";
+inserirTexto.innerHTML += "Este ";
+inserirTexto.innerHTML += "é ";
+inserirTexto.innerHTML += "o meu ";
+inserirTexto.innerHTML += "Exercicio ";
