5a6235ed8ad8f31cfee261695665e5a1e4ca78bd
xDominicz <julio.machsilva@gmail.com>
2022-08-01 19:38:42 -0300

    Exercicios DEVinHouse

---
+++ [M1S05]Ex05-ArrowFunction/README.md
@@ -0,0 +1,1 @@
+Utilize o if ternário para construir uma função que recebe o número do dias e retorna se pertence a primeira ou a segunda quinzena
---
+++ [M1S05]Ex05-ArrowFunction/script.js
@@ -0,0 +1,3 @@
+let verQuinzena = (dia) => (dia <= 15 ? console.log(`Dia ${dia} faz parte da primeira quinzena`) : console.log(`Dia ${dia} faz parte da segunda quinzena`));
+
+console.log(verQuinzena(16));
